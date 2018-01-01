4387c2428007e95952c5c58cf021810e9d63825d
Nefelijm <sistemasnik20@gmail.com>
2017-12-31 19:54:32 -0500

    Actualizando data

--- js/restaurant.js
+++ js/restaurant.js
@@ -12,6 +12,12 @@ $('document').ready(function(){
         $('#Rest2').text(Restaurant2);
         $('#Rest3').text(Restaurant3);
         $('#Rest4').text(Restaurant4);
+
+        $('#img1').attr('src', '../assets/img/yume.jpg');
+        $('#img2').attr('src', '../assets/img/madamTusam.jpg');
+        $('#img3').attr('src', '../assets/img/puntoAzul.jpg');
+        $('#img4').attr('src', '../assets/img/laRed.jpg');
+
 }
 
     if ($('#RestName').text() === 'HUANCAYO-SIERRA') {
@@ -24,6 +30,11 @@ $('document').ready(function(){
         $('#Rest2').text(Restaurant2);
         $('#Rest3').text(Restaurant3);
         $('#Rest4').text(Restaurant4);
+
+        $('#img1').attr('src','../assets/img/olimpico.jpg');
+        $('#img2').attr('src','../assets/img/tullpa.jpg');
+        $('#img3').attr('src', '../assets/img/padrino.jpg');
+        $('#img4').attr('src', '../assets/img/leopardo.jpg');
     }
 
     if ($('#RestName').text() === 'CHANCHAMAYO-SELVA') {
@@ -36,6 +47,11 @@ $('document').ready(function(){
         $('#Rest2').text(Restaurant2);
         $('#Rest3').text(Restaurant3);
         $('#Rest4').text(Restaurant4);
+
+        $('#img1').attr('src', '../assets/img/shekina.jpg');
+        $('#img2').attr('src', '../assets/img/mishaja.jpg');
+        $('#img3').attr('src', '../assets/img/tranquera.jpg');
+        $('#img4').attr('src', '../assets/img/shambari.jpg');
     }
    
 
