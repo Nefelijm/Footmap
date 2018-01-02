72d39b71c0fab80a6e4715344052457f764368d2
Nefelijm <sistemasnik20@gmail.com>
2018-01-02 12:21:22 -0500

    Modificando estilos de vistas

--- js/city.js
+++ js/city.js
@@ -1,32 +1,27 @@
-$('document').ready(function(){
-    
-    $('#city').on('input',function(){
-        var citys = Object.keys(data);
-        var limaCity = citys[0];
-        var huancayoCity = citys[1];
-        var selvaCity = citys[2]
-        var typeCitys = $('#city').val();
+$('document').ready(function() {
+  $('#city').on('input', function() {
+    var citys = Object.keys(data);
+    var limaCity = citys[0];
+    var huancayoCity = citys[1];
+    var selvaCity = citys[2];
+    var typeCitys = $('#city').val();
 
-        if (typeCitys === limaCity || typeCitys === huancayoCity || typeCitys === selvaCity) {
-          $('#restButton').removeAttr('disabled');
-            $('#msj1').text('');
-        
-      }
-      else{
-          $('#msj1').text('Escribe una de estas ciudades tal y como este escrito : LIMA-COSTA, HUANCAYO-SIERRA o CHANCHAMAYO-SELVA)');
-          $('#restButton').attr('disabled', 'disabled');      
-    
-      }
+    if (typeCitys === limaCity || typeCitys === huancayoCity || typeCitys === selvaCity) {
+      $('#restButton').removeAttr('disabled');
+      $('#msj1').text('');
+      $('#restButton').removeClass('color-disabled');
+      $('#restButton').addClass('color-enabled');
+    } else {
+      $('#msj1').text('Escribe una de estas ciudades tal y como este escrito : LIMA-COSTA, HUANCAYO-SIERRA o CHANCHAMAYO-SELVA)');
+      $('#restButton').attr('disabled', 'disabled');
+      $('#restButton').addClass('color-disabled');
+      $('#restButton').removeClass('color-enabled');
+    }
+  });
 
-     });
-
-     //Guardadndo los valores del input para la siguiente vista 
-    $('#restButton').click(function () {
-        localStorage.input = $('#city').val();     
-        window.location.href = '../views/restaurant.html';
-
-    });
-     
-
-
+  // Guardadndo los valores del input para la siguiente vista 
+  $('#restButton').click(function() {
+    localStorage.input = $('#city').val();     
+    window.location.href = '../views/restaurant.html';
+  });
 });
--- js/restaurant.js
+++ js/restaurant.js
@@ -1,103 +1,91 @@
-$('document').ready(function(){
-
- 
- $('#RestName').text(localStorage.input);
+$('document').ready(function() {
+  $('#RestName').text(localStorage.input);
     
-    if ($('#RestName').text() === 'LIMA-COSTA') {
-        var Restaurant1 = Object.keys(data['LIMA-COSTA'])[0];
-        var Restaurant2 = Object.keys(data['LIMA-COSTA'])[1];
-        var Restaurant3 = Object.keys(data['LIMA-COSTA'])[2];
-        var Restaurant4 = Object.keys(data['LIMA-COSTA'])[3];
+  if ($('#RestName').text() === 'LIMA-COSTA') {
+    var Restaurant1 = Object.keys(data['LIMA-COSTA'])[0];
+    var Restaurant2 = Object.keys(data['LIMA-COSTA'])[1];
+    var Restaurant3 = Object.keys(data['LIMA-COSTA'])[2];
+    var Restaurant4 = Object.keys(data['LIMA-COSTA'])[3];
 
-        $('#Rest1').text(Restaurant1);
-        $('#Rest2').text(Restaurant2);
-        $('#Rest3').text(Restaurant3);
-        $('#Rest4').text(Restaurant4);
+    $('#Rest1').text(Restaurant1);
+    $('#Rest2').text(Restaurant2);
+    $('#Rest3').text(Restaurant3);
+    $('#Rest4').text(Restaurant4);
 
-        $('#img1').attr('src', '../assets/img/yume.jpg');
-        $('#img2').attr('src', '../assets/img/madamTusam.jpg');
-        $('#img3').attr('src', '../assets/img/puntoAzul.jpg');
-        $('#img4').attr('src', '../assets/img/laRed.jpg');
+    $('#img1').attr('src', '../assets/img/yume.jpg');
+    $('#img2').attr('src', '../assets/img/madamTusam.jpg');
+    $('#img3').attr('src', '../assets/img/puntoAzul.jpg');
+    $('#img4').attr('src', '../assets/img/laRed.jpg');
 
-        mouseoverRestaurant();
-
-}
+    mouseoverRestaurant();
+  }
     
-    if ($('#RestName').text() === 'HUANCAYO-SIERRA') {
-        var Restaurant1 = Object.keys(data['HUANCAYO-SIERRA'])[0];
-        var Restaurant2 = Object.keys(data['HUANCAYO-SIERRA'])[1];
-        var Restaurant3 = Object.keys(data['HUANCAYO-SIERRA'])[2];
-        var Restaurant4 = Object.keys(data['HUANCAYO-SIERRA'])[3];
+  if ($('#RestName').text() === 'HUANCAYO-SIERRA') {
+    var Restaurant1 = Object.keys(data['HUANCAYO-SIERRA'])[0];
+    var Restaurant2 = Object.keys(data['HUANCAYO-SIERRA'])[1];
+    var Restaurant3 = Object.keys(data['HUANCAYO-SIERRA'])[2];
+    var Restaurant4 = Object.keys(data['HUANCAYO-SIERRA'])[3];
 
-        $('#Rest1').text(Restaurant1);
-        $('#Rest2').text(Restaurant2);
-        $('#Rest3').text(Restaurant3);
-        $('#Rest4').text(Restaurant4);
+    $('#Rest1').text(Restaurant1);
+    $('#Rest2').text(Restaurant2);
+    $('#Rest3').text(Restaurant3);
+    $('#Rest4').text(Restaurant4);
 
-        $('#img1').attr('src','../assets/img/olimpico.jpg');
-        $('#img2').attr('src','../assets/img/tullpa.jpg');
-        $('#img3').attr('src', '../assets/img/padrino.jpg');
-        $('#img4').attr('src', '../assets/img/leopardo.jpg');
-        mouseoverRestaurant();
-    }
+    $('#img1').attr('src', '../assets/img/olimpico.jpg');
+    $('#img2').attr('src', '../assets/img/tullpa.jpg');
+    $('#img3').attr('src', '../assets/img/padrino.jpg');
+    $('#img4').attr('src', '../assets/img/leopardo.jpg');
+    mouseoverRestaurant();
+  }
     
-    if ($('#RestName').text() === 'CHANCHAMAYO-SELVA') {
-        var Restaurant1 = Object.keys(data['CHANCHAMAYO-SELVA'])[0];
-        var Restaurant2 = Object.keys(data['CHANCHAMAYO-SELVA'])[1];
-        var Restaurant3 = Object.keys(data['CHANCHAMAYO-SELVA'])[2];
-        var Restaurant4 = Object.keys(data['CHANCHAMAYO-SELVA'])[3];
+  if ($('#RestName').text() === 'CHANCHAMAYO-SELVA') {
+    var Restaurant1 = Object.keys(data['CHANCHAMAYO-SELVA'])[0];
+    var Restaurant2 = Object.keys(data['CHANCHAMAYO-SELVA'])[1];
+    var Restaurant3 = Object.keys(data['CHANCHAMAYO-SELVA'])[2];
+    var Restaurant4 = Object.keys(data['CHANCHAMAYO-SELVA'])[3];
 
-        $('#Rest1').text(Restaurant1);
-        $('#Rest2').text(Restaurant2);
-        $('#Rest3').text(Restaurant3);
-        $('#Rest4').text(Restaurant4);
+    $('#Rest1').text(Restaurant1);
+    $('#Rest2').text(Restaurant2);
+    $('#Rest3').text(Restaurant3);
+    $('#Rest4').text(Restaurant4);
 
-        $('#img1').attr('src', '../assets/img/shekina.jpg');
-        $('#img2').attr('src', '../assets/img/mishaja.jpg');
-        $('#img3').attr('src', '../assets/img/tranquera.jpg');
-        $('#img4').attr('src', '../assets/img/shambari.jpg');
-        mouseoverRestaurant();
-    }
+    $('#img1').attr('src', '../assets/img/shekina.jpg');
+    $('#img2').attr('src', '../assets/img/mishaja.jpg');
+    $('#img3').attr('src', '../assets/img/tranquera.jpg');
+    $('#img4').attr('src', '../assets/img/shambari.jpg');
+    mouseoverRestaurant();
+  }
    
-/**********************MOUSEOVER**********/
+  /** ********************MOUSEOVER**********/
 
-    function mouseoverRestaurant(params) {
-       
-        $("#img1").mouseover(function () {
-            $('#Rest1').addClass("selected")
-        });
-        $("#img1").mouseout(function () {
-            $('#Rest1').removeClass("selected")
-        });
-        $("#img2").mouseover(function () {
-            $("#msjMouser").text('El restaurante ocupa la planta baja del Casal organizado en dos comedores,El comedor está pensado para albergar pequeñas celebraciones o comidas de empresa, con una capacidad de hasta ochenta personas.Además dispone de proyector y pantalla lo que posibilita su utilización para presentaciones y eventos similares')            
-        });
-        $("#img2").mouseout(function () {
-            $("#msjMouser").text('');
-        });
-        $("#img3").mouseover(function () {
-            $("#img3").css("width", "220px").slideUp(2000).slideDown(2000);
-        });
-        $("#img4").mouseover(function () {
-            alert('VISITANOS ESTAMOS A SU SERVICIO');
-        });
-
-    }
-
-/*************/
-    $('#Rest1,#Rest2, #Rest3, #Rest4').click(function () {
-
-        localStorage.ContRest = $('#Rest1').text();
-        localStorage.ContRest = $('#Rest2').text();
-        localStorage.ContRest = $('#Rest3').text();
-        localStorage.ContRest = $('#Rest4').text();
-
-        window.location.href = '../views/traditional-meal.html';
-
+  function mouseoverRestaurant(params) {
+    $('#img1').mouseover(function() {
+      $('#Rest1').addClass('selected');
     });
+    $('#img1').mouseout(function() {
+      $('#Rest1').removeClass('selected');
+    });
+    $('#img2').mouseover(function() {
+      $('#msjMouser').text('El restaurante ocupa la planta baja, con una capacidad de hasta ochenta personas.Además dispone de proyector y pantalla lo que posibilita su utilización para presentaciones y eventos similares');            
+    });
+    $('#img2').mouseout(function() {
+      $('#msjMouser').text('');
+    });
+    $('#img3').mouseover(function() {
+      $('#img3').css('width', '220px').slideUp(2000).slideDown(2000);
+    });
+    $('#img4').mouseover(function() {
+      alert('VISITANOS ESTAMOS A SU SERVICIO');
+    });
+  }
 
+  /** ***********/
+  $('#Rest1,#Rest2, #Rest3, #Rest4').click(function() {
+    localStorage.ContRest = $('#Rest1').text();
+    localStorage.ContRest = $('#Rest2').text();
+    localStorage.ContRest = $('#Rest3').text();
+    localStorage.ContRest = $('#Rest4').text();
 
-
-
-
+    window.location.href = '../views/traditional-meal.html';
+  });
 });
